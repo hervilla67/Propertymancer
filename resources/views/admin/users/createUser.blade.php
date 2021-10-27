b0fdf8d9fc8dbcfd2ea93b8ba180d208b127f53f
=hervilla67 <=hervilla67@gmail.com>
2021-10-26 21:31:22 -0400

    git add .

--- resources/views/admin/users/createUser.blade.php
+++ resources/views/admin/users/createUser.blade.php
@@ -5,9 +5,21 @@
 
     <article>
 
+<form action="{{ url('admin/users/create') }}">
+
+    <input type="text" name="username" id="username">
+    <input type="text" name="password" id="password">
+
+    <select name="select" id="select">
+
+        <option></option>
+
+    </select>
+
+</form>
 
 
-    </article>
+</article>
 
 </section>
 
